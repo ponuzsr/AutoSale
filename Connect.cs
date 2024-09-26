5e114bec59af867f5df57d13849fc7972d16e0b9
ponuzsr <ponuzsr@kkszki.hu>
2024-09-26 10:19:04 +0200

    Connect class ready to use

---
+++ Connect.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSale
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        private string Host;
+        private string Database;
+        private string User;
+        private string Password;
+        private string ConnectionString;
+
+        public Connect()
+        {
+            Host = "127.0.0.1";
+            Database = "Auto";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+
+        }
+    }
+}
